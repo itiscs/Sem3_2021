7439690301460c500b79f6e9af57ae0c6bd40afd
moudroff <moudroff>
2021-09-01 12:20:13 +0300

    Time

---
+++ TimeApp/TimeApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Time t1 = Time.CreateFromSec(10);
+            Console.WriteLine(t1);
+
+            Time t2 = Time.CreateFromTik(100);
+            Console.WriteLine(t2);
+            
+            Time t3 = Time.CreateFromMin(2);
+            Console.WriteLine(t3);
+
+            Console.WriteLine($"Sum = {t3 - t2 - t1}");
+
+
+            Time t4 = t1 + 5.00;
+            Console.WriteLine(t4);
+
+
+        }
+    }
+}
---
+++ TimeApp/TimeApp/Time.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeApp
+{
+    public class Time
+    {
+        private readonly double  sec;
+
+        public double Sec 
+        { 
+            get=>sec; 
+        }
+        public double Tik
+        {
+            get => sec*18.2;
+        }
+        public double Min
+        {
+            get => sec/60;
+        }
+
+        private Time()
+        {
+            sec = 0;
+        }
+
+        private Time(double s)
+        {
+            if (s < 0)
+                throw new ArgumentException("Negative time!!!");
+            sec = s;
+        }
+
+        public static Time CreateFromSec(double sec)
+        {
+            return new Time(sec);
+        }
+        public static Time CreateFromTik(double tik)
+        {
+            return new Time(tik/18.2);
+        }
+        public static Time CreateFromMin(double min)
+        {
+            return new Time(min*60);
+        }
+
+        public override string ToString()
+        {
+            return $"Sec-{Sec:0.00} Min-{Min:0.00} Tik - {Tik:0.00}";
+        }
+
+        public static Time operator+(Time t1, Time t2)
+        {
+            Time t3 = new Time(t1.sec + t2.sec);
+            return t3;
+        }
+        public static Time operator +(Time t1, double sec)
+        {
+            Time t3 = new Time(t1.sec + sec);
+            return t3;
+        }
+        public static Time operator &(Time t1, Time t2)
+        {
+            Time t3 = new Time(t1.sec - t2.sec);
+            return t3;
+        }
+
+
+
+
+
+    }
+}
