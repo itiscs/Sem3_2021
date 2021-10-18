b63652419c7c63d7156e26c42ec5845ea5675d50
moudroff <moudroff>
2021-10-18 15:34:54 +0300

    First Razor Pages

---
+++ FirstWebApp/FirstWebApp/Pages/Index.cshtml.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstWebApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public string Hello { get; set; }
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            Name = "Tom";
+            Age = 30;
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+        public void OnPost(string name, int? age)
+        {
+            Hello = $"Привет {name}, {age}!";
+
+        }
+    }
+}
---
+++ RegexpApp/RegexpApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace RegexpApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex reg = new Regex(@"\d{3,15}");
+
+            Regex reg2 = new Regex(@"^#?([A-Fa-f0-9]){3}(([A-Fa-f0-9]){3})?$");
+
+            Console.WriteLine(reg2.Match("#444444"));
+            
+
+            var str = "56576575876987973   he43llo";
+            
+            Console.WriteLine(reg.IsMatch(str));
+
+            foreach (var m in reg.Matches(str))
+            {
+                Console.WriteLine(m.ToString());
+
+            }
+
+
+
+        }
+    }
+}
