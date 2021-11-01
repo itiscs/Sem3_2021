b6f81914a4cecd876627ec4cfc22af40c914202d
moudroff <moudroff>
2021-11-01 15:26:10 +0300

    Редактирование страниц для Рейтинга

--- EntityApp/EntityApp/Models/Movie.cs
+++ EntityApp/EntityApp/Models/Movie.cs
@@ -11,13 +11,18 @@ namespace EntityApp.Models
         
         public int ID { get; set; }
         
+        [Display(Name = "Название")]
         public string Title { get; set; }
 
         [DataType(DataType.Date)]
+        [Display(Name = "Дата релиза")]
         public DateTime ReleaseDate { get; set; }
-
+        [Display(Name = "Жанр")]
         public string Genre { get; set; }
+        [Display(Name = "Цена")]
         public decimal Price { get; set; }
+        [Display(Name = "Рейтинг")]
+        [Range(0,10,ErrorMessage ="Рейтинг должен быть в интервале [0,10]")]
         public int Rating { get; set; }
     }
 }
